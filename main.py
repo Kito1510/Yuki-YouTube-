7aa489bdb0080cda6f934e57974c875abee033fe
Kito1510 <106915508+Kito1510@users.noreply.github.com>
2024-10-15 11:35:35 +0900

    Update main.py
    Update apis

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from cache import cache
 
 max_api_wait_time = 3
 max_time = 10
-apis = [r"https://inv.nadeko.net",r"https://invidious.fdn.fr/feed/popular",r"https://invidious.jing.rocks/feed/popular",r"https://invidious.privacyredirect.com/",r"https://invidious.jing.rocks/feed/popular",r"https://inv.nadeko.net/feed/popular"]
+apis = [r"https://inv.nadeko.net"]
 url = requests.get(r'https://raw.githubusercontent.com/mochidukiyukimi/yuki-youtube-instance/main/instance.txt').text.rstrip()
 version = "1.0"
 
